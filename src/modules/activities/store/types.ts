7bb8b1cfc3b5c6b5453a31f0b7ba139937d8c436
Hermine Adamyan <hermineadamyan.dev@gmil.com>
2018-05-25 11:38:17 +0400

    put only need data from state for activityMainDetails component

--- src/modules/activities/store/types.ts
+++ src/modules/activities/store/types.ts
@@ -20,6 +20,22 @@ export enum Category {
     Cognitive = 2,
 }
 
+export class ActivityMainDetailsInfo {
+    public dataExist: boolean = false;
+    public title?: string;
+    public description?: string;
+    public coverPhoto?: string;
+    public category?: Category;
+    public activityType?: ActivityType;
+    public mediaType?: MediaType;
+    public mediaTypeText?: string;
+    public mediaTypeIconClass?: string;
+    public orientation?: Orientation;
+    public orientationText?: string;
+    public orientationIconClass?: string;
+    public mediaCount?: number;
+}
+
 export interface IActivitiesState {
     activity: {
         details: {
@@ -30,7 +46,7 @@ export interface IActivitiesState {
             activityType: 2, // the activity type (e.g. wh questions , a good store) - should be defined as enum in the store types file (activityType always belong to a specifc category)
             mediaType: MediaType, // 0 = photos , 1 = videos - should be defined as enum in the store types file
             orientation: Orientation,
-            mediaCount: 12, // number of videos or photos in this activity
+            mediaCount: number, // number of videos or photos in this activity
             sessionsInfo: SessionsInfo[],
         },
         content: { },
--- src/views/activityDetails.vue
+++ src/views/activityDetails.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="ex-activity-details-component">
         <social-share></social-share>
-        <activity-main-details :activity="activityState"></activity-main-details>
+        <activity-main-details :activityMainDetailsInfo="activityMainDetailsInfo"></activity-main-details>
         <div class="ex-session-info mt-5 pt-3">
             <session-length :sessionLengthInfo="sessionsInfo"></session-length>
         </div>
@@ -10,14 +10,14 @@
 </template>
 
 <script lang="ts">
-    import { Component, Watch } from 'vue-property-decorator';
+    import { Component } from 'vue-property-decorator';
     import BaseComponent from '@/modules/common/components/baseComponent.vue';
     import {State, Action} from 'vuex-class';
     import ActivityMainDetails from '@/modules/activities/components/activitydetails/activityMainDetails.vue';
     import ImageGallery from '@/modules/common/components/imageGallery.vue';
     import SessionLength from '@/modules/activities/components/sessionLength.vue';
     import SocialShare from '@/modules/common/components/socialShare.vue';
-    import {IActivitiesState, SessionsInfo} from "@/modules/activities/store/types";
+    import {IActivitiesState, SessionsInfo, ActivityMainDetailsInfo, MediaType, Orientation} from "@/modules/activities/store/types";
 
     const namespace: string = 'activities';
 
@@ -40,15 +40,52 @@
 
        get sessionsInfo(): SessionsInfo[] {
             return this.activityState && this.activityState.details  &&  this.activityState.details.sessionsInfo;
-         }
+       }
+
+       get activityMainDetailsInfo(): ActivityMainDetailsInfo {
+            const detailsInfo = {
+               dataExist: false,
+               title: "",
+               description: "",
+               coverPhoto: "",
+               mediaCount: 0,
+               mediaType: 0,
+               mediaTypeText: "",
+               mediaTypeIconClass: "",
+               orientation: 0,
+               orientationText: "",
+               orientationIconClass: "",
+            };
+            if (this.activityState && this.activityState.details) {
+                detailsInfo.dataExist = true;
+                detailsInfo.title = this.activityState.details.title;
+                detailsInfo.description = this.activityState.details.description;
+                detailsInfo.coverPhoto = this.activityState.details.coverPhoto;
+                detailsInfo.mediaCount = this.activityState.details.mediaCount;
+                detailsInfo.mediaType = this.activityState.details.mediaType;
+                detailsInfo.orientation = this.activityState.details.orientation;
+
+                if (detailsInfo.mediaType === MediaType.Photo) {
+                    detailsInfo.mediaTypeText = this.$locale.activities.activityDetails.photoBasedText;
+                    detailsInfo.mediaTypeIconClass = "ex-photo";
+                } else if (detailsInfo.mediaType === MediaType.Video) {
+                    detailsInfo.mediaTypeText = this.$locale.activities.activityDetails.videoBasedText;
+                    detailsInfo.mediaTypeIconClass = "ex-play-video";
+                }
+
+                if (detailsInfo.orientation === Orientation.Landscape) {
+                    detailsInfo.orientationText = this.$locale.activities.activityDetails.landscapeText;
+                    detailsInfo.orientationIconClass = "ex-landscape";
+                } else if (detailsInfo.orientation === Orientation.Portrait) {
+                    detailsInfo.orientationText = this.$locale.activities.activityDetails.portraitText;
+                    detailsInfo.orientationIconClass = "ex-portrait";
+                }
+            }
+            return detailsInfo;
+       }
+
         public created() {
             this.getActivity({activity: "1"});
-            // please remove after you test
-            setTimeout(() => {
-                 //this.activityState.details = null;
-                 //this.activityState.details.sessionsInfo[0].slidesCount = 111;
-                 // this.activityState.details.sessionsInfo.push({id: 44 , description: 'temp', slidesCount: 88});
-            }, 5000);
         }
     }
 </script>
