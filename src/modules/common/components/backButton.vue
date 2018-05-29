e906b0865637fe43c3ba10354e808b2a1cd49b8b
Hermine Adamyan <hermineadamyan.dev@gmil.com>
2018-05-29 16:40:40 +0400

    back button component

---
+++ src/modules/common/components/backButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-flex xs3>
+        <v-btn flat icon color="dark" class="ml-0" @click="$router.go(-1)">
+            <i class="ex-back-arrow-icon"></i>
+        </v-btn>
+    </v-flex>
+</template>
+
+<script lang="ts">
+    import { Component } from 'vue-property-decorator';
+    import BaseComponent from '@/modules/common/components/baseComponent.vue';
+
+    @Component
+    export default class BackButton extends BaseComponent {
+
+    }
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
