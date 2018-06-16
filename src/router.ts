2f67170e3d72d998aa22e3c23b46cb38e506735a
Zvi Carni <sagikarni76@gmail.com>
2018-06-16 22:03:42 +0300

    layout bug in full screen

--- src/router.ts
+++ src/router.ts
@@ -50,8 +50,12 @@ export default new Router({
             name: 'FullScreenView',
             component: FullScreenView,
             beforeEnter: (to, from, next) => {
-                bus.$emit(busConstants.ENTER_FULL_SCREEN);
                 next();
+                // delay full screen in 200 milisec
+                // this solves a layout bug caused by openeing full screen while rendering the new page at thhe same time
+                setTimeout(() => {
+                    bus.$emit(busConstants.ENTER_FULL_SCREEN);
+                }, 200);
             },
             children: [
                 {
