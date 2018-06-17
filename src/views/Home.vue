b71ed4cf554694069e2cbc688d69870f1229de75
Zvi Carni <sagikarni76@gmail.com>
2018-06-17 23:15:47 +0300

    add video activity data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <p>This is a home page.</p>
+       <v-flex>
+        <v-btn @click="$router.push('activity-details/1')">Premium - single photo</v-btn>
+          <v-btn @click="$router.push('activity-details/2')">Premium - 2 horizontal photos</v-btn>
+            <v-btn @click="$router.push('activity-details/3')">Premium - 2 vertical photo</v-btn>
+              <v-btn @click="$router.push('activity-details/4')">Premium - Four photos</v-btn>
+
+               <v-btn @click="$router.push('activity-details/100')">Premium - Server Error</v-btn>
+               <v-btn @click="$router.push('activity-details/100')">Premium - Not Found</v-btn>
+
+       </v-flex>
+
     </div>
 </template>
 
