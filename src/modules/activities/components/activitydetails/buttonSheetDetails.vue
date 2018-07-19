c5fd9b4b62a703456245f4dc5e62dd5eb1973df2
developer <hermineadamyan.dev@gmail.com>
2018-07-19 20:27:58 +0400

    Started Zoom Activity

---
+++ src/modules/activities/components/activitydetails/buttonSheetDetails.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-app id="inspire">
+        <div>
+            <v-icon>lightbulb-on-outline</v-icon>
+            <v-bottom-sheet inset>
+                <v-btn
+                        slot="activator"
+                        color="red"
+                        dark
+                >
+                <i class="lightbulb-on-outline"></i>
+                </v-btn>
+                <v-card tile>
+                    <v-progress-linear
+                            :value="50"
+                            class="my-0"
+                            height="3"
+                    ></v-progress-linear>
+
+                    <v-list>
+                        <v-list-tile>
+                            <v-list-tile-content>
+                                <v-list-tile-title>The Walker</v-list-tile-title>
+                                <v-list-tile-sub-title>Fitz & The Trantrums</v-list-tile-sub-title>
+                            </v-list-tile-content>
+
+                            <v-spacer></v-spacer>
+
+                            <v-list-tile-action>
+                                <v-btn icon>
+                                    <v-icon>fast_rewind</v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
+
+                            <v-list-tile-action :class="{ 'mx-5': $vuetify.breakpoint.mdAndUp }">
+                                <v-btn icon>
+                                    <v-icon>pause</v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
+
+                            <v-list-tile-action :class="{ 'mr-3': $vuetify.breakpoint.mdAndUp }">
+                                <v-btn icon>
+                                    <v-icon>fast_forward</v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
+                        </v-list-tile>
+                    </v-list>
+                </v-card>
+            </v-bottom-sheet>
+        </div>
+    </v-app>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Watch } from 'vue-property-decorator';
+    import BaseComponent from '@/modules/common/components/baseComponent.vue';
+
+    @Component
+    export default class ButtonSheetDetails extends BaseComponent {
+
+    }
+</script>
+
+<style scoped lang="scss">
+    #inspire{
+        height: 0!important;
+        .application--wrap{
+            min-height: 0;
+        }
+    }
+
+</style>
---
+++ src/modules/activities/components/slideShowBased/zoomSlide.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div class="full-height table">
+            <div class="cell">
+                <img style="height: 100%; width: 100%; background-color: white"  class="object-fit_contain" :src="selectPhotoMedia(parameter.media.photos[0])">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop } from 'vue-property-decorator';
+    import BaseComponent from '@/modules/common/components/baseComponent.vue';
+    import {PremiumCollectionLayout} from '@/modules/activities/store/types';
+
+    @Component
+    export default class ZoomSlide extends BaseComponent {
+        @Prop() public parameter?: any;
+
+        public pauseAction(): void {
+            // do nothing
+        }
+        public stopAction(): void {
+            // do nothing
+        }
+        public revertWitpModal(): void {
+            // do nothing
+        }
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .object-fit_contain { object-fit: contain }
+    .full-height{
+        height:100%;
+        background:#F8F8F8;
+    }
+    .full-height.two{
+        background:#ACACAC;
+    }
+    .full-height.three{
+        background:#5E5E5E;
+    }
+    .table{
+        display:table;
+        width:100%;
+        position:relative;
+        background: transparent!important;
+    }
+    .cell {
+        display: table-cell;
+        vertical-align: middle;
+        width: 100%;
+        margin: 0 auto;
+        height:100%;
+        text-align: center;
+    }
+    .row{
+        display:table-row;
+        width:auto;
+    }
+    .fourCell{
+        background-color: white;
+        width: calc(50% - 60px);
+        padding: 5px 5px 0px 5px;
+        box-sizing: border-box;
+    }
+    .twoCell{
+        background-color: white;
+        width: calc(100% - 40px);
+        margin: 5px 5px
+    }
+
+</style>
