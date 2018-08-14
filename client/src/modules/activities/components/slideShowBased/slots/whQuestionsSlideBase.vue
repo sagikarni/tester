198a7c42e72dfa6ebb64d92d65de78fa9fa6b0b8
Hermine Adamyan <hermineadamyan.dev@gmail.com>
2018-08-14 19:38:51 +0400

    Finished whQuestionSlide move to slot

--- client/src/modules/activities/components/slideShowBased/slots/whQuestionsSlideBase.vue
+++ client/src/modules/activities/components/slideShowBased/slots/whQuestionsSlideBase.vue
@@ -4,12 +4,12 @@
             <div class="cell">
                 <div class="cell-content">
                     <slot></slot>
-                    <div v-if="question" class="wh-question" @click="changeQuestion">
+                    <div v-if="questionsStatus" class="wh-question" @click="changeQuestion">
                         <div class="text_refresh_wrapper">
                             <span class="refresh_icon">
                                 <i class="material-icons meaningRefresh"> cached </i>
                             </span>
-                            <span class="spanFade meaningSpanFade">{{questionsArray[0]}}</span>
+                            <span class="spanFade meaningSpanFade">{{questions[0]}}</span>
                         </div>
                     </div>
                 </div>
@@ -23,10 +23,132 @@
     import BaseComponent from '@/modules/common/components/baseComponent.vue';
     import {PremiumCollectionLayout, ImageType} from '@/modules/activities/store/types';
 
+    import TimelineMax from 'gsap';
+    const timeLineMax = TimelineMax as any;
 
     @Component
     export default class WHQuestionsSlideBase extends BaseComponent {
+        @Prop() public question?: boolean;
+        @Prop() public questionsText?: any[];
+        public transitionEnded = true;
+
+
+        get questionsStatus(): boolean | undefined {
+            return this.question;
+        }
+
+        get questions(): any[] | undefined {
+            return this.questionsText;
+        }
+
+        public getNextQuestion() {
+            if (this.questionsText) {
+                const firstQ = this.questionsText.shift();
+                this.questionsText.push(firstQ);
+            }
+        }
+
+        public changeQuestion() {
+            if (!this.transitionEnded) {
+                return;
+            }
+
+            const elemrefresh = document.getElementsByClassName("meaningRefresh");
+            const elemFadingWord = document.getElementsByClassName("meaningSpanFade");
+
+            this.transitionEnded = false;
+            timeLineMax.set(elemrefresh, {transform: "rotate(180deg)", autoAlpha: 0.5 });
+
+            timeLineMax.to(elemFadingWord, 0.5,
+                {
+                    autoAlpha: 0,
+                    ease: Power1.easeOut,
+                    onComplete: () => {
+                        timeLineMax.set(elemrefresh, {transform: "rotate(0deg)", autoAlpha: 1 });
+                        this.getNextQuestion();
+                        timeLineMax.to(elemFadingWord, 0.5, {
+                            autoAlpha: 1, ease: Power1.easeOut, onComplete: () => {
+                                this.transitionEnded = true;
+                            },
+                        });
+                    },
+                });
+        }
 
     }
 
 </script>
+
+
+
+<style scoped lang="scss">
+    .object-fit_contain { object-fit: contain }
+    .full-height{
+        height:100%;
+        background:#F8F8F8;
+    }
+    .table{
+        display:table;
+        width:100%;
+        position:relative;
+        background: transparent!important;
+    }
+
+    .cell {
+        display: table-cell;
+        vertical-align: middle;
+        width: 100%;
+        margin: 0 auto;
+        height: 100%;
+        text-align: center;
+        .cell-content {
+            position: relative;
+            max-height: 100vh !important;
+        }
+    }
+
+    .wh-question {
+        cursor: pointer;
+        position: absolute;
+        padding: 7px 55px 1px 100px;
+        color: white;
+        bottom: 16px;
+        left: 0;
+        text-align: left;
+        box-sizing: border-box;
+        font-size: 18px;
+        background: rgba(0, 0, 0, 0.6);
+        width: 100%;
+        .text_refresh_wrapper {
+            display: inline-block;
+            position: relative;
+            min-width: 150px;
+        }
+        span {
+            font-size: 24px;
+            min-width: 150px;
+            display: inline-block;
+            float: left;
+            line-height: 32px;
+            padding-left: 80px;
+        }
+        .refresh_icon {
+            position: absolute;
+            top: -7px;
+            left: 0;
+            width: 40px;
+            min-width: 55px;
+            background: #000000;
+            margin-right: 0;
+            padding-left: 0;
+        }
+        i {
+            font-size: 32px;
+            float: right;
+            margin: 0 10px;
+            margin: 8px 10px;
+            transition: all 0.5s;
+        }
+    }
+
+</style>
--- client/src/modules/activities/components/slideShowBased/slots/whQuestionsVideoSlide.vue
+++ client/src/modules/activities/components/slideShowBased/slots/whQuestionsVideoSlide.vue
@@ -1,6 +1,130 @@
  <template>
-    <div>
-            <video-player class="vjs-custom-box vjs-big-play-centered">
-            </video-player>      
-    </div>
-</template>
+     <div>
+         <wh-questions-slide-base :question="question" :questionsText="questionsArray">
+             <video-player class="vjs-custom-box vjs-big-play-centered">
+             </video-player>
+         </wh-questions-slide-base>
+     </div>
+ </template>
+
+ <script lang="ts">
+     import { Component, Prop } from 'vue-property-decorator';
+     import BaseComponent from '@/modules/common/components/baseComponent.vue';
+     import {PremiumCollectionLayout, ImageType} from '@/modules/activities/store/types';
+     import whQuestionsSlideBase from '@/modules/activities/components/slideShowBased/slots/whQuestionsSlideBase.vue';
+
+
+     @Component({
+         components: {
+             whQuestionsSlideBase,
+         },
+     })
+     export default class WHQuestionsPhotoSlide extends BaseComponent {
+         @Prop() public parameter?: any;
+         public question: boolean = false;
+
+         get questionsArray(): any[] {
+             return this.parameter && this.parameter.media && this.parameter.media.questions;
+         }
+         get getMediaTypes(): any {
+             return ImageType;
+         }
+         public isValid(): boolean {
+             return this.parameter && this.parameter.media && this.parameter.media.questions && this.parameter.media.questions.length > 0;
+         }
+
+         public created() {
+             if (this.isValid()) {
+                 this.question = true;
+             }
+         }
+
+         public pauseAction(): void {
+             // do nothing
+         }
+         public stopAction(): void {
+             // do nothing
+         }
+         public revertWitpModal(): void {
+             // do nothing
+         }
+         public showFirstShape() {
+             // do nothing
+         }
+     }
+
+ </script>
+
+
+ <style scoped lang="scss">
+     .object-fit_contain { object-fit: contain }
+     .full-height{
+         height:100%;
+         background:#F8F8F8;
+     }
+     .table{
+         display:table;
+         width:100%;
+         position:relative;
+         background: transparent!important;
+     }
+
+     .cell {
+         display: table-cell;
+         vertical-align: middle;
+         width: 100%;
+         margin: 0 auto;
+         height: 100%;
+         text-align: center;
+         .cell-content {
+             position: relative;
+             max-height: 100vh !important;
+         }
+     }
+
+     .wh-question {
+         cursor: pointer;
+         position: absolute;
+         padding: 7px 55px 1px 100px;
+         color: white;
+         bottom: 16px;
+         left: 0;
+         text-align: left;
+         box-sizing: border-box;
+         font-size: 18px;
+         background: rgba(0, 0, 0, 0.6);
+         width: 100%;
+         .text_refresh_wrapper {
+             display: inline-block;
+             position: relative;
+             min-width: 150px;
+         }
+         span {
+             font-size: 24px;
+             min-width: 150px;
+             display: inline-block;
+             float: left;
+             line-height: 32px;
+             padding-left: 80px;
+         }
+         .refresh_icon {
+             position: absolute;
+             top: -7px;
+             left: 0;
+             width: 40px;
+             min-width: 55px;
+             background: #000000;
+             margin-right: 0;
+             padding-left: 0;
+         }
+         i {
+             font-size: 32px;
+             float: right;
+             margin: 0 10px;
+             margin: 8px 10px;
+             transition: all 0.5s;
+         }
+     }
+
+ </style>
+
