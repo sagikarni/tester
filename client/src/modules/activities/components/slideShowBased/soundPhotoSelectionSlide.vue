d261980ecbb429c44d3b27476f4d63601126ea97
Hermine Adamyan <hermineadamyan.dev@gmail.com>
2018-08-20 16:52:14 +0400

    Added  new speaker

--- client/src/modules/activities/components/slideShowBased/soundPhotoSelectionSlide.vue
+++ client/src/modules/activities/components/slideShowBased/soundPhotoSelectionSlide.vue
@@ -9,8 +9,10 @@
                          :src="getImagePath(parameter.photos[1], getMediaTypes.Content )"/>
                 </div>
             </div>
-            <audio :src="getImagePath(this.parameter.sound)" :id="`audio_id_${audioIndex}`"></audio>
-            <v-icon class="sound-up" @click="playSound">volume_up</v-icon>
+            <audio :src="getImagePath(this.parameter.sound)" :ref="`audio_id_${audioIndex}`"></audio>
+            <div class="speaker-container">
+                <div class="speaker-wrapper"><div @click="playSound" class="speaker stop"></div></div>
+            </div>
             <div class="row">
                 <div class=" cell">
                     <img class="object-fit_contain fourCell" data-id='3' @click="checkIndex($event)"
@@ -38,11 +40,11 @@
         public audioObj: any;
         public ifSoundPlayed: boolean = false;
         public play: boolean = false;
-        public soundEnd: boolean = false;
+        public setTime: any;
 
         public mounted() {
-            this.audioObj = document.querySelector(`#audio_id_${this.audioIndex}`);
-            this.soundEnd = this.audioObj.ended;
+            const name = `audio_id_${this.audioIndex}`;
+            this.audioObj = this.$refs[name];
         }
 
         get getMediaTypes(): any {
@@ -53,22 +55,22 @@
             return this.slideIndex;
         }
 
-        @Watch('soundEnd')
-        public onPropertyChanged(value: boolean, oldValue: boolean) {
-            if (value) {
-                this.soundEnd = false;
-                this.play = false;
-            }
-        }
-
         public playSound() {
             if (!this.play) {
                 this.audioObj.play();
                 this.ifSoundPlayed = true;
                 this.play = true;
+                timeLineMax.set('.speaker', { className: "-=stop"});
+                const duration = (this.audioObj.duration - this.audioObj.currentTime) * 1000;
+                this.setTime = setTimeout(() => {
+                    this.play = false;
+                    timeLineMax.set('.speaker', { className: "+=stop"});
+                }, duration);
             } else {
                 this.audioObj.pause();
                 this.play = false;
+                timeLineMax.set('.speaker', { className: "+=stop"});
+                clearTimeout(this.setTime);
             }
         }
 
@@ -76,6 +78,8 @@
             this.audioObj.pause();
             this.audioObj.currentTime = 0;
             this.play = false;
+            timeLineMax.set('.speaker', { className: "+=stop"});
+            clearTimeout(this.setTime);
         }
 
         public checkIndex(event: any) {
@@ -141,11 +145,17 @@
     }
 
     .fourCell {
-        background-color: white;
         width: calc(50% - 60px);
-        padding: 5px 5px 0px 5px;
+        margin: 5px 5px 0 5px;
+        background-color: white;
+        border: solid 2px transparent;
         -webkit-filter: brightness(1);
         filter: brightness(1);
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        transition: all 0.3s ease;
+        -webkit-transition: all 0.3s ease;
     }
 
     .twoCell {
@@ -173,21 +183,113 @@
     }
 
     .greenBorder {
-        border: solid 2px green;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
+        border: solid 2px green!important;
     }
 
     .redBorder {
-        border: solid 2px red;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
+        border: solid 2px red!important;
     }
 
     .nonBorder {
         border: none;
     }
 
+    .speaker-container {
+        position: absolute;
+        width: 140px;
+        height: 140px;
+        background-color: #000000;
+        border-radius: 50%;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 9999;
+    }
+    .speaker-wrapper {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .speaker {
+        border-width: 15px 26px 15px 0;
+        height: 50px;
+        position: relative;
+        border-top-right-radius: 10px 50px;
+        border-bottom-right-radius: 10px 50px;
+    }
+    .speaker, .speaker:before, .speaker:after {
+        border-color: transparent #ffffff transparent transparent;
+    }
+    .speaker, .speaker:before {
+        border-style: solid;
+        width: 0;
+    }
+    .speaker:before, .speaker:after {
+        content: "";
+        position: absolute;
+    }
+    .speaker:before {
+        right: 0;
+        bottom: 0;
+        height: 40px;
+        border-width: 5px 40px 5px 0;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+    }
+    .speaker:after {
+        height: 50px;
+        width: 30px;
+        left: -7px;
+        top: -20px;
+        border-radius: 50%;
+        border-style: double;
+        border-width: 20px;
+        -webkit-animation: play 0.3s infinite alternate linear;
+        -moz-animation: play 0.3s infinite alternate linear;
+        animation: play 0.3s infinite alternate linear;
+    }
+
+
+    .stop:after {
+        @extend .speaker:after;
+        -webkit-animation: play 0.9s 1 alternate linear;
+        -moz-animation: play 0.9s 1 alternate linear;
+        animation: play 0.9s 1 alternate linear;
+    }
+
+
+    @-webkit-keyframes play {
+        from {
+            -webkit-transform: scale(1);
+        }
+        to {
+            -webkit-transform: scale(1.1);
+        }
+    }
+    @-moz-keyframes play {
+        from {
+            -moz-transform: scale(1);
+        }
+        to {
+            -moz-transform: scale(1.1);
+        }
+    }
+    @keyframes play {
+        from {
+            -webkit-transform: scale(1);
+            -moz-transform: scale(1);
+            -ms-transform: scale(1);
+            -o-transform: scale(1);
+            transform: scale(1);
+        }
+        to {
+            -webkit-transform: scale(1.1);
+            -moz-transform: scale(1.1);
+            -ms-transform: scale(1.1);
+            -o-transform: scale(1.1);
+            transform: scale(1.1);
+        }
+    }
 </style>
